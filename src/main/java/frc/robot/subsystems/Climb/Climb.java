385ae64d6831201d64eb381ca698abe1591254b6
eb1ta1 <ebitaiume@gmail.com>
2024-03-03 10:04:27 +0900

    fix climb code

--- src/main/java/frc/robot/subsystems/Climb/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb/Climb.java
@@ -5,12 +5,7 @@
 package frc.robot.subsystems.Climb;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.SparkMaxAlternateEncoder;
-import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -18,46 +13,11 @@ public class Climb extends SubsystemBase {
 
     final CANSparkMax leaderMotor = new CANSparkMax(Constants.Climb.leaderCanId, MotorType.kBrushless);
     final CANSparkMax followerMotor = new CANSparkMax(Constants.Climb.followerCanId, MotorType.kBrushless);
-    private static final SparkMaxAlternateEncoder.Type kAltEncType = SparkMaxAlternateEncoder.Type.kQuadrature;
-    private static final int kCPR = 8192;
-    private RelativeEncoder alternateEncoder;
-    private SparkPIDController pidController;
-    public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
 
     public Climb() {
-        alternateEncoder = leaderMotor.getAlternateEncoder(kAltEncType, kCPR);
-        pidController = leaderMotor.getPIDController();
-        pidController.setFeedbackDevice(alternateEncoder);
-
         leaderMotor.restoreFactoryDefaults();
         followerMotor.restoreFactoryDefaults();
         followerMotor.follow(leaderMotor);
-
-        // PID parameters
-        kP = 0.1;
-        kI = 1e-4;
-        kD = 1;
-        kIz = 0;
-        kFF = 0;
-        kMaxOutput = 1;
-        kMinOutput = -1;
-
-        // set PID parameters
-        pidController.setP(kP);
-        pidController.setI(kI);
-        pidController.setD(kD);
-        pidController.setIZone(kIz);
-        pidController.setFF(kFF);
-        pidController.setOutputRange(kMinOutput, kMaxOutput);
-
-        // display PID coefficients on SmartDashboard
-        SmartDashboard.putNumber("P Gain", kP);
-        SmartDashboard.putNumber("I Gain", kI);
-        SmartDashboard.putNumber("D Gain", kD);
-        SmartDashboard.putNumber("I Zone", kIz);
-        SmartDashboard.putNumber("Feed Forward", kFF);
-        SmartDashboard.putNumber("Max Output", kMaxOutput);
-        SmartDashboard.putNumber("Min Output", kMinOutput);
     }
 
     public void loosen() {
@@ -68,45 +28,6 @@ public class Climb extends SubsystemBase {
         leaderMotor.set(Constants.Climb.tightenSpeed);
     }
 
-    // foo() { foo(0, "undefined"); }
     public void setSetpoint(int rotations) {
-        // read PID coefficients from SmartDashboard
-        double p = SmartDashboard.getNumber("P Gain", 0);
-        double i = SmartDashboard.getNumber("I Gain", 0);
-        double d = SmartDashboard.getNumber("D Gain", 0);
-        double iz = SmartDashboard.getNumber("I Zone", 0);
-        double ff = SmartDashboard.getNumber("Feed Forward", 0);
-        double max = SmartDashboard.getNumber("Max Output", 0);
-        double min = SmartDashboard.getNumber("Min Output", 0);
-
-        // if PID parameters on SmartDashboard have changed, write new values to
-        // controller
-        if ((p != kP)) {
-            pidController.setP(p);
-            kP = p;
-        }
-        if ((i != kI)) {
-            pidController.setI(i);
-            kI = i;
-        }
-        if ((d != kD)) {
-            pidController.setD(d);
-            kD = d;
-        }
-        if ((iz != kIz)) {
-            pidController.setIZone(iz);
-            kIz = iz;
-        }
-        if ((ff != kFF)) {
-            pidController.setFF(ff);
-            kFF = ff;
-        }
-        if ((max != kMaxOutput) || (min != kMinOutput)) {
-            pidController.setOutputRange(min, max);
-            kMinOutput = min;
-            kMaxOutput = max;
-        }
-
-        pidController.setReference(rotations, CANSparkMax.ControlType.kPosition);
     }
 }
